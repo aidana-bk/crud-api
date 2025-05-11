d9b471e38d9ce78939331ad577769bea6a0b1582
Aidana K <aidana.amnbk@gmail.com>
2025-05-11 18:58:05 +0500

    feat: use GET api/users to get all users

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { v4 as uuidv4 } from "uuid";
 
 dotenv.config();
 
@@ -8,6 +9,25 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 
+const users = [
+  {
+    id: uuidv4(),
+    username: "John Doe",
+    age: 30,
+    hobbies: ["reading", "traveling"],
+  },
+  {
+    id: uuidv4(),
+    username: "Jane Smith",
+    age: 25,
+    hobbies: ["painting", "cycling"],
+  },
+];
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get("/api/users", (_, res) => {
+  res.status(200).json(users);
 });
